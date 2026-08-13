1b16e86ca2eb74e345137decc80641c707cdf17e
unknown <sistelca@hotmail.com>
2026-08-13 05:17:29 -0500

    codigo

--- services/database.py
+++ services/database.py
@@ -2,7 +2,9 @@ import sqlite3
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_NAME = os.path.join(BASE_DIR, 'notebook.db')
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+DB_NAME = os.path.join(DATA_DIR, 'notebook.db')
+
 
 def init_db():
     conn = sqlite3.connect(DB_NAME)
